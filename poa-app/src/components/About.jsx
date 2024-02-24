331fd8e1f0bf04b975b0118c5d30422842cf792a
Emma Nasseri <enasseri02@gmail.com>
2024-02-24 10:50:28 -0700

    basic styling and architecture

---
+++ poa-app/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>Learn more about our organization and what we do.</p>
+      {/* Add more content here as needed */}
+    </div>
+  );
+};
+
+export default About;
--- poa-app/src/pages/_app.js
+++ poa-app/src/pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
