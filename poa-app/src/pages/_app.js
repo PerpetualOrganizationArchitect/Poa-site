30a98d49acc94442c9637c2016c04e424859f73c
Emma Nasseri <enasseri02@gmail.com>
2024-02-24 11:08:56 -0700

    changed default font to roboto mono

--- poa-app/src/pages/_app.js
+++ poa-app/src/pages/_app.js
@@ -1,8 +1,22 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme, CSSReset } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "'Roboto Mono', monospace", // Use Roboto Mono for headings
+    body: "'Roboto Mono', monospace", // Use Roboto Mono for body text
+  },
+  styles: {
+    global: {
+      body: {
+        bgGradient: "linear(to-r, orange.100, pink.100)", // Using Chakra UI's color tokens
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
