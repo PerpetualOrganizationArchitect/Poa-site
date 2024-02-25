de13c37d4defc462f8d9e5e643187d6609eb79ce
Emma Nasseri <enasseri02@gmail.com>
2024-02-24 19:10:37 -0700

    added input box

--- poa-app/src/pages/architect/index.js
+++ poa-app/src/pages/architect/index.js
@@ -1,11 +1,28 @@
 // src/pages/about/index.js
 import React from "react";
 import Layout from "../../components/Layout";
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  IconButton,
+} from "@chakra-ui/react";
+import { ArrowUpIcon } from "@chakra-ui/icons";
 
 const ArchitectPage = () => {
   return (
     <Layout>
       <p>architect page</p>
+      <InputGroup>
+        <Input pr="4.5rem" placeholder="Type here..." borderColor="blue.900" />
+        <InputRightElement>
+          <IconButton
+            icon={<ArrowUpIcon color="teal" />}
+            variant="ghost"
+            aria-label="Home"
+          />
+        </InputRightElement>
+      </InputGroup>
     </Layout>
   );
 };
