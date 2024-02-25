5d2828249d0febede9f00b02de7656983ebe25b6
hudsonhrh <hudsonhrh7@gmail.com>
2024-02-25 16:51:34 -0700

    minor chnages

--- poa-app/contracts/MembershipNFTFactory.sol
+++ poa-app/contracts/MembershipNFTFactory.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.20;
-import "./membership.sol";
+
+import "./MembershipNFT.sol";
 
 contract NFTMembershipFactory {
     address[] public deployedContracts;
