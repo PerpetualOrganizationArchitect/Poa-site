3fa4a647766443c48775ada1b2287866061fc5e8
hudsonhrh <hudsonhrh7@gmail.com>
2024-02-26 16:29:05 -0700

    added gating to minting of participation token

--- poa-app/contracts/ParticipationToken.sol
+++ poa-app/contracts/ParticipationToken.sol
@@ -4,12 +4,31 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+interface INFTMembership {
+    function checkMemberTypeByAddress(address user) external view returns (string memory);
+}
+
 contract ParticipationToken is ERC20, Ownable {
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    INFTMembership public nftMembership;
+
+    mapping(string => bool) public allowedRoles;
+
+    constructor(string memory name, string memory symbol, address _nftMembership, string[] memory _allowedRoleNames) ERC20(name, symbol) {
+        nftMembership = INFTMembership(_nftMembership);
+
+        for (uint256 i = 0; i < _allowedRoleNames.length; i++) {
+            allowedRoles[_allowedRoleNames[i]] = true;
+        }
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    modifier canMint() {
+        string memory memberType = nftMembership.checkMemberTypeByAddress(msg.sender);
+        require(allowedRoles[memberType], "Not authorized to mint coins");
+        _;
+    }
+
+    function mint(address to, uint256 amount) public canMint {
         _mint(to, amount);
     }
 
