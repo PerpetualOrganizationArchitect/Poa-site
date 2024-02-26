eb611f3c3e5be011981786760ea40b30adc6626f
hudsonhrh <hudsonhrh7@gmail.com>
2024-02-26 16:25:20 -0700

    added membership role gating to ddtoken mint

--- poa-app/contracts/DirectDemocracyTokenFactory.sol
+++ poa-app/contracts/DirectDemocracyTokenFactory.sol
@@ -7,8 +7,8 @@ contract DirectDemocracyTokenFactory {
     event TokenCreated(address tokenAddress, string name, string symbol);
 
 
-    function createDirectDemocracyToken(string memory name, string memory symbol) public {
-        DirectDemocracyToken newToken = new DirectDemocracyToken(name, symbol);
+    function createDirectDemocracyToken(string memory name, string memory symbol, address _nftMembership, string[] memory _allowedRoleNames) public {
+        DirectDemocracyToken newToken = new DirectDemocracyToken(name,  symbol, _nftMembership, _allowedRoleNames);
 
         newToken.transferOwnership(msg.sender);
         emit TokenCreated(address(newToken), name, symbol);
