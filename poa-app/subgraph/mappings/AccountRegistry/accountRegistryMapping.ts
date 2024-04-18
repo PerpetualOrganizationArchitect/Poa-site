e3a7072fb4e716f911d5a030036b4d8173bbaaa9
hudsonhrh <hudsonhrh7@gmail.com>
2024-04-17 22:49:32 -0500

    added account registry subgraph stuff

---
+++ poa-app/subgraph/mappings/AccountRegistry/accountRegistryMapping.ts
@@ -0,0 +1,27 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  AccountManager,
+  UserRegistered,
+  UsernameChanged
+} from "../../generated/UniversalAccountRegistry/AccountManager"
+import { Account } from "../../generated/schema"
+
+export function handleUserRegistered(event: UserRegistered): void {
+  let account = new Account(event.params.accountAddress.toHex())
+  account.address = event.params.accountAddress
+  account.userName = event.params.username
+  account.save()
+}
+
+export function handleUsernameChanged(event: UsernameChanged): void {
+  let id = event.params.accountAddress.toHex()
+  let account = Account.load(id)
+  
+  if (account == null) {
+    account = new Account(id)
+    account.address = event.params.accountAddress
+  }
+
+  account.userName = event.params.newUsername
+  account.save()
+}
