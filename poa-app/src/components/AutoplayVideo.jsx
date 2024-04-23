dfd4d7d66bf59a3c88a2622d562287aa78db4a27
hudsonhrh <hudsonhrh7@gmail.com>
2024-04-23 00:50:42 -0500

    main page chnages

---
+++ poa-app/src/components/AutoplayVideo.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from 'react';
+import { Box, AspectRatio } from '@chakra-ui/react';
+
+const AutoPlayVideo = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.muted = true; // Mute the video to ensure autoplay
+      setTimeout(() => video.play(), 5000); // Small delay before playing
+    }
+  }, []); // Only run when the component mounts
+
+  return (
+    <Box mt="7%" w ="69%">
+        <video
+          ref={videoRef}
+          autoPlay
+          loop
+          controls
+          playsInline
+          muted
+          src="/video/demo.mp4" // Check if this path is correct
+        />
+    </Box>
+  );
+};
+
+export default AutoPlayVideo;
--- poa-app/src/pages/index.js
+++ poa-app/src/pages/index.js
@@ -8,6 +8,9 @@ import { TypeAnimation } from "react-type-animation";
 import { Flex, Box, Button, Text } from "@chakra-ui/react";
 import MissionStatement from "@/components/MissionStatement";
 
+import AutoPlayVideo from '@/components/AutoplayVideo';
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -23,11 +26,18 @@ export default function Home() {
         direction="column"
         alignItems="center"
         justifyContent="center"
-        h="100vh" // Set the height of the Flex container to 100% of the viewport height
-        p={10}
+        h="200vh" 
+        p={7}
+        
       >
-        <Text fontSize="70" color="black.900">
-          Poa.
+        <Text fontSize="99" color="black.900" mt={"7%"}>
+          Poa
+        </Text>
+        <Text mb="2" mt="-6" fontSize="18px" color="gray.600">
+        pʌ oʊ ə
+      </Text>
+        <Text mb="8" fontSize="17" color="black.900" >
+          Perpetual Organization Architect
         </Text>
         <TypeAnimation
           sequence={[
@@ -41,19 +51,37 @@ export default function Home() {
             "where community is fully yours, forever.",
             1000,
           ]}
-          speed={28}
-          style={{ fontSize: "2em" }}
+          speed={33}
+          style={{ fontSize: "2.5em" }}
           repeat={0}
         />
 
         <Link href="/landing" passHref>
           {" "}
           {/* Use Link to navigate */}
-          <Button size="lg" colorScheme="blue" variant="outline" mt={4}>
+          <Button size={"lg"}  colorScheme="blue" variant="outline" mt={8}>
             Explore Poa
           </Button>
         </Link>
+        <AutoPlayVideo />
+
+        <Text fontWeight={"bold"} fontSize="33" color="black.900" mt={"6%"}>
+          What is a Perpetual Organization?
+        </Text>
+        <Text fontSize="21" color="black.900" mt="7" mb="" w="555px">
+        Perpetual Organizations are fully decentralized, unstoppable, and community-owned Organizations based on contribution and democracy not investment. The community vote controls every aspect of the organization from the code to the treasury with no reliance on centralized authorities.
+        </Text>
+        <Link href="/about" passHref>
+        <Button size={"lg"} colorScheme="blue" variant="outline" mt={8}>
+          Learn More
+        </Button>
+        </Link>
       </Flex>
+      <Box justifyContent="center" alignItems={"center"}>
+
+      </Box>
+
+
     </>
   );
 }
