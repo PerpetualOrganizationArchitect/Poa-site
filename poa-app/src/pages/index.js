c31cc9c3b837d5305b27e49e6950aea33c9ff33a
hudsonhrh <hudsonhrh7@gmail.com>
2024-04-23 01:11:15 -0500

    fixed more for mobile

--- poa-app/src/pages/index.js
+++ poa-app/src/pages/index.js
@@ -1,17 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import * as React from "react";
 import Link from "next/link";
-import { TypeAnimation } from "react-type-animation";
-
 import { Flex, Box, Button, Text } from "@chakra-ui/react";
-import MissionStatement from "@/components/MissionStatement";
-
-import AutoPlayVideo from '@/components/AutoplayVideo';
-
-
-const inter = Inter({ subsets: ["latin"] });
+import { TypeAnimation } from "react-type-animation";
+import AutoPlayVideo from "@/components/AutoPlayVideo";
 
 export default function Home() {
   return (
@@ -26,22 +17,21 @@ export default function Home() {
         direction="column"
         alignItems="center"
         justifyContent="center"
-        h="200vh" 
+        h="200vh"
         p={7}
-        
       >
-        <Text fontSize="99" color="black.900" mt={"7%"}>
+        <Text fontSize={["77px", "88px", "99px"]} color="black.900" mt="7%">
           Poa
         </Text>
-        <Text mb="2" mt="-6" fontSize="18px" color="gray.600">
-        pʌ oʊ ə
-      </Text>
-        <Text mb="8" fontSize="17" color="black.900" >
+        <Text mb="2" mt="-6" fontSize={["14px", "16px", "18px"]} color="gray.600">
+          pʌ oʊ ə
+        </Text>
+        <Text mb="8" fontSize={["13px", "15px", "17px"]} color="black.900">
           Perpetual Organization Architect
         </Text>
+
         <TypeAnimation
           sequence={[
-            // Same substring at the start will only be typed once, initially
             "where community is decentralized.",
             1000,
             "where community is the future.",
@@ -52,36 +42,31 @@ export default function Home() {
             1000,
           ]}
           speed={33}
-          style={{ fontSize: "2.5em" }}
+          style={{ fontSize: "2em" }}
           repeat={0}
         />
 
         <Link href="/landing" passHref>
-          {" "}
-          {/* Use Link to navigate */}
-          <Button size={"lg"}  colorScheme="blue" variant="outline" mt={8}>
+          <Button size={"lg"} colorScheme="blue" variant="outline" mt={8}>
             Explore Poa
           </Button>
         </Link>
+
         <AutoPlayVideo />
 
-        <Text fontWeight={"bold"} fontSize="33" color="black.900" mt={"6%"}>
+        <Text fontSize={["20px", "25px", "30px", "33px"]} color="black.900" mt="6%">
           What is a Perpetual Organization?
         </Text>
-        <Text fontSize="21" color="black.900" mt="7" mb="" w="555px">
-        Perpetual Organizations are fully decentralized, unstoppable, and community-owned Organizations based on contribution and democracy not investment. The community vote controls every aspect of the organization from the code to the treasury with no reliance on centralized authorities.
+        <Text fontSize={["18px", "20px", "21px"]} color="black.900" mt="7" w={["95%", "85%", "7%", "555px"]} >
+          Perpetual Organizations are fully decentralized, unstoppable, and community-owned organizations based on contribution and democracy, not investment.
         </Text>
+
         <Link href="/about" passHref>
-        <Button size={"lg"} colorScheme="blue" variant="outline" mt={8}>
-          Learn More
-        </Button>
+          <Button size={"lg"} colorScheme="blue" variant="outline" mt={8}>
+            Learn More
+          </Button>
         </Link>
       </Flex>
-      <Box justifyContent="center" alignItems={"center"}>
-
-      </Box>
-
-
     </>
   );
 }
