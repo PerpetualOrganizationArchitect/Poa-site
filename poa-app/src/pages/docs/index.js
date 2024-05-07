84cd1a3744bbd4299fd36707e51abc40a28e3b41
hudsonhrh <hudsonhrh7@gmail.com>
2024-05-07 13:42:54 -0500

    added images

--- poa-app/src/pages/docs/index.js
+++ poa-app/src/pages/docs/index.js
@@ -1,15 +1,15 @@
 import Link from 'next/link';
 import { getSortedPostsData } from '../../util/posts';
-import { Text, Flex, VStack, Box, Divider, HStack } from '@chakra-ui/react';
+import { Text, Flex, VStack, Box, Divider, HStack, Image } from '@chakra-ui/react';
 import Layout from '@/components/Layout';
 
 export default function Home({ allPostsData }) {
     console.log(allPostsData);
     return (
         <Layout>
-            <Flex ml="6%">
+            <Flex ml="3%">
                 <Box mt="120px">
-                    <VStack mt="2" align="flex-start">
+                    <VStack mt="1" align="flex-start">
                         <Text fontWeight="600" fontSize="22px">Articles</Text>
                         <Divider color="black" />
                         {allPostsData.map(({ id }) => (
@@ -32,13 +32,13 @@ export default function Home({ allPostsData }) {
                     </VStack>
                 </Box>
                 <Box
-                    ml="9%"
-                    mt="120px"
+                    ml="8%"
+                    mt="100px"
                     bg="rgba(0, 0, 0, 0.75)"
                     p="4"
                     borderRadius="xl"
                     maxWidth="fit-content"
-                    mr="10%"
+                    mr="9%"
                 >
                     <VStack align="flex-start">
                         <Text
@@ -49,19 +49,29 @@ export default function Home({ allPostsData }) {
                         >
                             Get Started with Poa
                         </Text>
-                        <HStack mt="2">
+                        <HStack >
                             <Link href="/create">
-                                <Box
-                                    _hover={{ transform: "scale(1.03)", transition: "transform 0.3s" }}
-                                    mt="4"
-                                    p="4"
-                                    borderRadius="lg"
-                                    bg="rgba(0, 0, 0, 0.65)"
-                                >
-                                    <Text fontWeight="600" textColor="white" fontSize="20px">
-                                        How to Create a Perpetual Organization
-                                    </Text>
-                                </Box>
+                            <Box
+                                _hover={{ transform: "scale(1.03)", transition: "transform 0.3s" }}
+                                mt="4"
+                                p="4"
+                                borderRadius="lg"
+                                bg="rgba(0, 0, 0, 0.65)"
+                                display="flex"
+                                flexDirection="column"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <Text fontWeight="600" textColor="white" fontSize="20px" >
+                                    How to Create a Perpetual Organization
+                                </Text>
+                                <Image
+                                    mt="1"
+                                    src="/images/high_res_poa.png"
+                                    alt="Poa Logo"
+                                    height={[100,140, 180]}
+                                />
+                            </Box>
                             </Link>
                             <Link href="/join">
                                 <Box
@@ -70,10 +80,20 @@ export default function Home({ allPostsData }) {
                                     p="4"
                                     borderRadius="lg"
                                     bg="rgba(0, 0, 0, 0.65)"
+                                    display="flex"
+                                    flexDirection="column"
+                                    alignItems="center"
+                                    justifyContent="center"
                                 >
                                     <Text fontWeight="600" textColor="white" fontSize="20px">
                                         What is a Perpetual Organization?
                                     </Text>
+                                    <Image
+                                        src="/images/Po.webp"
+                                        alt="Poa Logo"
+                                        mt="1"
+                                        height={[100,140, 180]}
+                                    />
                                 </Box>
                             </Link>
                             <Link href="/manage">
@@ -83,10 +103,20 @@ export default function Home({ allPostsData }) {
                                     p="4"
                                     borderRadius="lg"
                                     bg="rgba(0, 0, 0, 0.65)"
+                                    display="flex"
+                                    flexDirection="column"
+                                    alignItems="center"
+                                    justifyContent="center"
                                 >
                                     <Text fontWeight="600" textColor="white" fontSize="20px">
                                         How to Join a Perpetual Organization
                                     </Text>
+                                    <Image
+                                        mt="1"
+                                        src="/images/join.webp"
+                                        alt="AlphaV1"
+                                        height={[100,140, 180]}
+                                    />
                                 </Box>
                             </Link>
                         </HStack>
@@ -99,7 +129,7 @@ export default function Home({ allPostsData }) {
                             Featured Articles
                         </Text>
                         <HStack mt="2">
-                            <Link href="/create">
+                            <Link href="/docs/AlphaV1">
                                 <Box
                                     maxW={"50%"}
                                     _hover={{ transform: "scale(1.03)", transition: "transform 0.3s" }}
@@ -107,10 +137,21 @@ export default function Home({ allPostsData }) {
                                     p="4"
                                     borderRadius="lg"
                                     bg="rgba(0, 0, 0, 0.65)"
+                                    display="flex"
+                                    flexDirection="column"
+                                    alignItems="center"
+                                    justifyContent="center"
                                 >
                                     <Text fontWeight="600" textColor="white" fontSize="18px">
                                         AlphaV1: The First Step Towards the Future of Decentralized Organizations
                                     </Text>
+                                    <Image
+                                        mt="2"
+                                        src="/images/alphaV1.webp"
+                                        alt="AlphaV1"
+                                
+                                        height={[120,160, 220]}
+                                    />
                                 </Box>
                             </Link>
                         </HStack>
