56e7c692bf9022e017a353bfe2f520ff6e558c53
hudsonhrh <hudsonhrh7@gmail.com>
2024-05-07 14:38:20 -0500

    new article

---
+++ poa-app/posts/create.md
@@ -0,0 +1,31 @@
+# Creating a Perpetual Organization with Poa
+
+Welcome to the future of decentralized organization management, where Poa, your AI-powered chatbot assistant, guides you through the intricate process of setting up your own Perpetual Organization. 
+
+![Poa](https://ipfs.io/ipfs/QmbgTrMGLBEKoqUG9PQoT8M3FoTGuQXxxApyRPt45XEdwK "Poa")
+
+
+The journey begins with Poa asking you basic yet crucial details about your new organization. You'll start by selecting an organization name, logo, and a concise description that captures the essence of your mission. Next, Poa walks you through the advanced feature selection.
+
+## Deciding on Voting Mechanisms
+
+One of the core features of any organization is its voting system, and with Poa, you have the flexibility to choose from several innovative options:
+
+- **Hybrid Voting**: This method allows you to balance between contribution-based voting and direct democracy, tailoring how each is weighted in decision-making processes. [Learn about why this is useful](#).
+- **Contribution-based Voting**: Your members' voting power correlates with their activity and contributions to the organization, ensuring that those who invest effort have a greater say.
+- **Direct Democracy**: Each member has an equal vote, promoting fairness and equality within your organization.
+
+Additionally, you can set a voting quorum percentage and decide whether to implement quadratic voting, enhancing the democratic quality by curbing the influence of large stakeholders.
+
+
+## Task and Role Management
+
+Poa empowers you to define specific roles within your organization, assigning permissions that align with your operational needs. Each role can have different capabilities such as creating various types of votes (direct democracy, contribution-based, or hybrid), managing tasks, reviewing tasks, and updating site information.
+
+## Smart Contract and Treasury Management
+
+An advanced feature that Poa assists with is determining who can update smart contracts and manage the organization’s treasury. This crucial decision influences how your organization adapts and secures its financial and operational integrity over time.
+
+## Conclusion
+
+Poa the Perpetual Organization Architect transforms the complex process of creating and managing a decentralized organization into an interactive and user-friendly experience. With Poa's guidance, you're not just setting up an organization; you're future-proofing it to be adaptable, transparent, and aligned with your community's values and contributions.
--- poa-app/src/pages/docs/index.js
+++ poa-app/src/pages/docs/index.js
@@ -8,37 +8,42 @@ export default function Home({ allPostsData }) {
     return (
         <Layout>
             <Flex ml="3%">
-                <Box mt="120px">
-                    <VStack mt="1" align="flex-start">
-                        <Text fontWeight="600" fontSize="22px">Articles</Text>
+            <Box mt="120px">
+                    <VStack mt="1" align="flex-start" gap="3">
+                        <Text fontWeight="700" fontSize="20px">Get Started</Text>
                         <Divider color="black" />
-                        {allPostsData.map(({ id }) => (
-                            <Link href={`/blog/${id}`} key={id} style={{ textDecoration: 'none' }}>
-                                <Text
-                                    fontWeight="400"
-                                    fontSize="20px"
-                                    _hover={{
-                                        textDecoration: "none",
-                                        transform: "scale(1.05)"
-                                    }}
-                                    transition="transform 0.2s"
-                                    display="block"
-                                    width="fit-content"
-                                >
-                                    {id.replace(/-/g, ' ')}
-                                </Text>
-                            </Link>
-                        ))}
+                        <Link href={`/docs/create`} style={{ textDecoration: 'none' }}>
+                            <Text  _hover={{ transform: "scale(1.03)", transition: "transform 0.3s" }} fontWeight="500" fontSize="15px">Creating a Perpetual Organization</Text>
+                        </Link>
+                        <Link href={`/docs/perpetualOrganization`} style={{ textDecoration: 'none' }}>
+                            <Text  _hover={{ transform: "scale(1.03)", transition: "transform 0.3s" }}  fontWeight="500" fontSize="15px">What is a Perpetual Organization?</Text>
+                        </Link>
+                        <Link href={`/docs/join`} style={{ textDecoration: 'none' }}>
+                            <Text  _hover={{ transform: "scale(1.03)", transition: "transform 0.3s" }} fontWeight="500" fontSize="15px">Joining a Perpetual Organization</Text>
+                        </Link>
+
+                        <Text fontWeight="700" fontSize="20px">Voting</Text>
+                        <Divider color="black" />
+                        <Link href={`/blog/hybridVoting`} style={{ textDecoration: 'none' }}>
+                            <Text  _hover={{ transform: "scale(1.03)", transition: "transform 0.3s" }} fontWeight="500" fontSize="15px">Hybrid Voting</Text>
+                        </Link>
+                        <Link href={`/docs/contributionVoting`} style={{ textDecoration: 'none' }}>
+                            <Text  _hover={{ transform: "scale(1.03)", transition: "transform 0.3s" }} fontWeight="500" fontSize="15px">Contribution Based Voting</Text>
+                        </Link>
+                        <Link href={`/blog/ddVoting`} style={{ textDecoration: 'none' }}>
+                            <Text  _hover={{ transform: "scale(1.03)", transition: "transform 0.3s" }} fontWeight="500" fontSize="15px">Direct Democracy Voting</Text>
+                        </Link>
+                        
                     </VStack>
                 </Box>
                 <Box
-                    ml="8%"
+                    ml="2%"
                     mt="100px"
-                    bg="rgba(0, 0, 0, 0.75)"
+                    bg="rgba(0, 0, 0, 0.8)"
                     p="4"
                     borderRadius="xl"
                     maxWidth="fit-content"
-                    mr="9%"
+                    mr="7%"
                 >
                     <VStack align="flex-start">
                         <Text
@@ -47,7 +52,7 @@ export default function Home({ allPostsData }) {
                             maxWidth="400px"
                             textColor="white"
                         >
-                            Get Started with Poa
+                            Learn about Poa
                         </Text>
                         <HStack >
                             <Link href="/create">
@@ -128,7 +133,7 @@ export default function Home({ allPostsData }) {
                             >
                             Featured Articles
                         </Text>
-                        <HStack mt="2">
+                        <HStack >
                             <Link href="/docs/AlphaV1">
                                 <Box
                                     maxW={"50%"}
@@ -150,7 +155,7 @@ export default function Home({ allPostsData }) {
                                         src="/images/alphaV1.webp"
                                         alt="AlphaV1"
                                 
-                                        height={[120,160, 220]}
+                                        height={[100,140, 180]}
                                     />
                                 </Box>
                             </Link>
