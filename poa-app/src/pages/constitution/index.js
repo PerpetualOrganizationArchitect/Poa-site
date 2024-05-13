73a27e2575ce5a8c8c5b89c3664f22610fe9365d
hudsonhrh <hudsonhrh7@gmail.com>
2024-05-13 17:51:55 -0500

    inital constiution page formatted well now needs more description

--- poa-app/src/pages/constitution/index.js
+++ poa-app/src/pages/constitution/index.js
@@ -2,30 +2,54 @@ import React, { useEffect, useState } from "react";
 import Navbar from "@/templateComponents/studentOrgDAO/NavBar";
 import { useGraphContext } from "@/context/graphContext";
 import Link from "next/link";
-import { Flex, VStack, Box, Text, Button } from "@chakra-ui/react";
+import { Flex, VStack, Box, Text, Button, Heading } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 function generatePODetails(poData) {
-    const { HybridVoting = null, DirectDemocracyVoting = null, ParticipationVoting = null, NFTMembership = null, Treasury = null } = poData.perpetualOrganization;
+    const {
+        HybridVoting = null,
+        DirectDemocracyVoting = null,
+        ParticipationVoting = null,
+        NFTMembership = null,
+        Treasury = null
+    } = poData.perpetualOrganization;
+
     let descriptions = [];
-    if (HybridVoting) {
-        descriptions.push(`Hybrid Voting System (ID: ${HybridVoting.id}) requires a quorum of ${HybridVoting.quorum}% of total votes.`);
-    }
-    if (DirectDemocracyVoting) {
-        descriptions.push(`Direct Democracy Voting System (ID: ${DirectDemocracyVoting.id}) requires a quorum of ${DirectDemocracyVoting.quorum}% of total votes.`);
-    }
-    if (ParticipationVoting) {
-        descriptions.push(`Participation Voting System (ID: ${ParticipationVoting.id}) requires a quorum of ${ParticipationVoting.quorum}% of total votes.`);
-    }
+    let treasuryControl = "an unidentified voting system";
+
+    descriptions.push(<Heading size="lg" >Voting Types</Heading>);
+
+    const addVotingSystemDescription = (name, system) => {
+        if (system) {
+            descriptions.push(<Heading size="md" mt="2">{name}</Heading>);
+            descriptions.push(<Text>Description: Requires a quorum of {system.quorum}% of total votes.</Text>);
+            // Check if this system controls the treasury
+            if (Treasury && Treasury.votingContract === system.id) {
+                treasuryControl = name;
+            }
+        }
+    };
+
+    addVotingSystemDescription("Hybrid Voting", HybridVoting);
+    addVotingSystemDescription("Direct Democracy Voting", DirectDemocracyVoting);
+    addVotingSystemDescription("Participation Voting", ParticipationVoting);
+
+    descriptions.push(<Heading size="lg" mt="4">Treasury</Heading>);
+    const votingControlDescription = <Text>The treasury is controlled by the {treasuryControl} System.</Text>;
+    descriptions.push(votingControlDescription);
+
     if (NFTMembership) {
-        descriptions.push(`Executive Roles: ${NFTMembership.executiveRoles.join(', ')}.`);
-        descriptions.push(`Member Types: ${NFTMembership.memberTypeNames.join(', ')}.`);
+        descriptions.push(<Heading size="lg" mt="4">Roles</Heading>);
+        descriptions.push(<Heading size="md" mt="2">Executive Roles</Heading>);
+        descriptions.push(<Text>{NFTMembership.executiveRoles.join(', ')}</Text>);
+        descriptions.push(<Heading size="md" mt="2">Member Types</Heading>);
+        descriptions.push(<Text>{NFTMembership.memberTypeNames.join(', ')}</Text>);
     }
-    if (Treasury) {
-        descriptions.push(`The treasury is controlled by the voting contract at address ${Treasury.votingContract}.`);
-    }
-    return descriptions.join("\n");
+
+    return descriptions;
 }
+
+
 
 const ConstitutionPage = () => {
     const { fetchRules } = useGraphContext();
@@ -33,51 +57,48 @@ const ConstitutionPage = () => {
     const { userDAO } = router.query;
 
     // Manage description as a state
-    const [description, setDescription] = useState("Loading...");
+    const [descriptionElements, setDescriptionElements] = useState([]);
 
     useEffect(() => {
-        // Define an async function inside useEffect
         const fetchData = async () => {
             let poData = await fetchRules(userDAO);
-            console.log("poData", poData);
-            setDescription(generatePODetails(poData));
+            setDescriptionElements(generatePODetails(poData));
         };
-        // Call the function
-        fetchData();
-    }, [userDAO]); // Dependency array to re-run the effect when userDAO changes
+        if (userDAO) {
+            fetchData();
+        }
+    }, [userDAO]);
 
     return (
         <>
-        <Navbar/>
-
-                <Text fontSize="xl" fontWeight="bold">Perpetual Organization Constitution</Text>
-                <Box>
-                    <Text>{description}</Text>
+            <Navbar/>
+            <VStack spacing={4} align="center">
+                <Heading mt="2" as="h1" size="xl" fontWeight="bold">Perpetual Organization Constitution</Heading>
+                <Box textColor={"white"} p="20px"  borderRadius="lg" overflow="hidden" bg="rgba(0, 0, 0, 0.85)">
+                    {descriptionElements}
                 </Box>
-                <Box>
-                    <Link href={`/user/?userDAO=${userDAO}`} passHref>
-                        <Button
-                            bgGradient="linear(to-r, teal.300, green.300)"
-                            color="white"
-                            _hover={{
-                                bgGradient: "linear(to-r, teal.600, green.600)",
-                            }}
-                            _active={{
-                                bgGradient: "linear(to-r, teal.700, green.700)",
-                            }}
-                            borderRadius="full"
-                            px="6"
-                            py="2"
-                            fontSize="lg"
-                            fontWeight="bold"
-                            textColor="black"
-                        >
-                            Dashboard
-                        </Button>
-                    </Link>
-                </Box>
+                <Link href={`/user/?userDAO=${userDAO}`} passHref>
+                    <Button
+                        bgGradient="linear(to-r, teal.300, green.300)"
+                        color="white"
+                        _hover={{
+                            bgGradient: "linear(to-r, teal.600, green.600)",
+                        }}
+                        _active={{
+                            bgGradient: "linear(to-r, teal.700, green.700)",
+                        }}
+                        borderRadius="full"
+                        px="6"
+                        py="2"
+                        fontSize="lg"
+                        fontWeight="bold"
+                        textColor="black"
+                    >
+                        Dashboard
+                    </Button>
+                </Link>
+            </VStack>
         </>
-  
     );
 };
 
