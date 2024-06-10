cd8cf39e9c4eff1053e3c0731c2b7f8a9efcb4fa
hudsonhrh <hudsonhrh7@gmail.com>
2024-06-10 11:20:33 -0500

    fixed ipfs issues by pinning using the graph api

--- poa-app/src/context/ipfsContext.js
+++ poa-app/src/context/ipfsContext.js
@@ -1,27 +1,22 @@
 import { create } from 'ipfs-http-client';
 import { createContext, useContext, useState, useEffect } from 'react';
 
-
-
 const IPFScontext = createContext();
 
-export const useIPFScontext = () =>{
-    return useContext(IPFScontext)
+export const useIPFScontext = () => {
+    return useContext(IPFScontext);
 }
 
-
-export const IPFSprovider = ({children}) =>{
+export const IPFSprovider = ({children}) => {
     async function addToIpfs(content) {
         try {
-    
-          const addedData = await ipfs.add({ content });
-    
-          return addedData;
+            const addedData = await ipfs.add({ content });
+            return addedData;
         } catch (error) {
-          console.error("An error occurred while adding to IPFS:", error);
-          throw error; 
+            console.error("An error occurred while adding to IPFS:", error);
+            throw error; 
         }
-      }
+    }
 
     async function fetchFromIpfs(ipfsHash) {
         let stringData = '';
@@ -36,32 +31,20 @@ export const IPFSprovider = ({children}) =>{
         }
     }
     
-    const auth = 'Basic ' + Buffer.from(process.env.NEXT_PUBLIC_INFURA_PROJECTID + ':' + process.env.NEXT_PUBLIC_INFURA_IPFS).toString('base64');
-
     const ipfs = create({
-    host: 'ipfs.infura.io',
-    port: 5001,
-    protocol: 'https',
-    apiPath: '/api/v0',
-    headers: {
-        authorization: auth,
-    },
-
+        host: 'api.thegraph.com',
+        port: 443,
+        protocol: 'https',
+        apiPath: '/ipfs/api/v0' 
     });
 
-    return(
-        <IPFScontext.Provider
-        value ={{
+    return (
+        <IPFScontext.Provider value={{
             ipfs,
             fetchFromIpfs,
             addToIpfs,
-        }}
-        >
-        {children}
+        }}>
+            {children}
         </IPFScontext.Provider>
-
-    )
+    );
 }
-
-
-
