022c3177cfad59271fb1c7a18d444176ee41dd19
hudsonhrh <hudsonhrh7@gmail.com>
2024-07-08 14:11:26 -0500

    dashbaord about info formatted properly

--- poa-app/src/pages/dashboard/index.js
+++ poa-app/src/pages/dashboard/index.js
@@ -12,7 +12,6 @@ import {
   Badge,
   Link, 
   Image,
-
 } from '@chakra-ui/react';
 import { useWeb3Context } from '@/context/web3Context';
 import { useGraphContext } from '@/context/graphContext';
@@ -85,181 +84,200 @@ const PerpetualOrgDashboard = () => {
         animation: `${glowAnimation} alternate 2.1s ease-in-out`,
       };
 
-  return (
-    <>
-      <Navbar />
-      <Box p={4}>
-        <Grid
-          color="white"
-          templateAreas={[
-            `'orgInfo orgInfo' 'tasks polls' 'leaderboard constitution'`,
-            `'orgInfo orgInfo' 'tasks polls' 'leaderboard constitution'`
-          ]}
-          templateColumns="repeat(2, 1fr)"
-          gap={4}
-        >
-          <GridItem area={'orgInfo'}>
-            <Box
-              w="100%"
-              borderRadius="2xl"
-              bg="transparent"
-              boxShadow="lg"
-              position="relative"
-              zIndex={2}
+      return (
+        <>
+          <Navbar />
+          <Box p={4}>
+            <Grid
+              color="white"
+              templateAreas={[
+                `'orgInfo orgInfo' 'tasks polls' 'leaderboard constitution'`,
+                `'orgInfo orgInfo' 'tasks polls' 'leaderboard constitution'`
+              ]}
+              templateColumns="repeat(2, 1fr)"
+              gap={4}
             >
-              <div style={glassLayerStyle} />
-              <VStack pb={1} position="relative" borderTopRadius="2xl" align="flex-start">
-                <div style={glassLayerStyle} />
-                <HStack spacing={4}>
-                  <Text pl={6} letterSpacing="-1%" fontSize="4xl" fontWeight="bold">
-                    {userDAO}'s Dashboard 
-                  </Text>
-                </HStack>
-              </VStack>
-              <Grid templateColumns="repeat(2, 1fr)" w="100%" gap={6}>
-                <Box>
-                <VStack  align="flex-start" ml="4">
-                <Image src={imageURL} alt="Organization Logo" width="150px" />
-                
-                <VStack spacing={2} align="flex-start">
-                  <HStack ml="4" spacing={2} align="center">
-                    <Icon as={FaInfoCircle} boxSize={5} />
-                    <Text fontWeight={"bold"} fontSize="2xl" mt={2}>
-                      Description: 
-                    </Text>
-                  </HStack>
-                  <Text mt="-1"fontSize="lg" ml="10">
-                    {poDescription}
-                  </Text>
-                </VStack>
-                
-                </VStack>
-                </Box>
-                <Box>
-                <HStack spacing={2} align="center">
-                    <Icon as={FaLink} boxSize={5} />
-                    <Text fontSize="2xl" fontWeight="bold">
-                      About Links
-                    </Text>
-                </HStack>
-                
-                <VStack mb="4" ml="6">
-                {poLinks && poLinks.length > 0 ? (
-                  poLinks.map((link, index) => (
-                    <Text key={index} fontSize="lg">
-                      <Link fontWeight={"bold"} href={link.url} passHref isExternal color="blue.400">
-                        {link.name}
-                      </Link>
-                    </Text>
-                  ))
-                ) : (
-                  <Text fontSize="lg" mt={2}>No links available</Text>
-                )}
-                </VStack>
-                </Box>
-                </Grid>
-            </Box>
-          </GridItem>
-
-          <GridItem area={'tasks'}>
-            <Box
-              w="100%"
-              borderRadius="2xl"
-              bg="transparent"
-              boxShadow="lg"
-              position="relative"
-              zIndex={2}
-            >
-              <div style={glassLayerStyle} />
-              <VStack pb={1} align="flex-start" position="relative" borderTopRadius="2xl">
-                <div style={glassLayerStyle} />
-                <Text pl={6} fontWeight="bold" fontSize="2xl">
-                  {claimedTasks && claimedTasks.length > 0 ? 'Your Claimed Tasks' : 'Recommended Tasks'}
-                </Text>
-              </VStack>
-              <HStack spacing="3.5%" pb={2} ml={4} mr={4} pt={4}>
-                {((claimedTasks && claimedTasks.length > 0) ? claimedTasks : reccomendedTasks)?.slice(0, 3).map((task) => (
-                  <Box key={task.id} w="31%" _hover={{ boxShadow: "md", transform: "scale(1.07)"}} p={4} borderRadius="2xl" overflow="hidden" bg="black">
-                    <Link2 href={`/tasks/?task=${task.id}&projectId=${task.projectId}&userDAO=${userDAO}`}>
-                      <VStack textColor="white" align="stretch" spacing={3}>
-                        <Text fontSize="md" lineHeight="99%" fontWeight="extrabold">
-                          {task.id}
-                        </Text>
-                        <HStack justify="space-between">
-                          <Badge colorScheme="yellow">{task.difficulty}</Badge>
-                          <Text fontWeight="bold">Tokens {task.payout}</Text>
+              <GridItem area={'orgInfo'}>
+                <Box
+                  w="100%"
+                  borderRadius="2xl"
+                  bg="transparent"
+                  boxShadow="lg"
+                  position="relative"
+                  zIndex={2}
+                >
+                  <div style={glassLayerStyle} />
+                  <VStack pb={1} position="relative" borderTopRadius="2xl" align="flex-start">
+                    <div style={glassLayerStyle} />
+                    <HStack spacing={4}>
+                      <Text pl={6} letterSpacing="-1%" fontSize="4xl" fontWeight="bold">
+                        {userDAO}'s Dashboard 
+                      </Text>
+                    </HStack>
+                  </VStack>
+                  <Grid templateColumns="repeat(3, 1fr)" w="100%" gap={6} >
+                    <Box>
+                      <VStack mt="4" spacing={2} >
+                      <Image src={imageURL} alt="Organization Logo" width="170px" />
+                        <HStack spacing={2} align="center">
+                          <Text fontWeight={"bold"} fontSize="2xl" mt={0}>
+                            Description: 
+                          </Text>
                         </HStack>
+                        <Text mb="2" mt="-1" fontSize="lg" ml="2">
+                          {poDescription}
+                        </Text>
                       </VStack>
-                    </Link2>
-                  </Box>
-                ))}
-              </HStack>
-            </Box>
-          </GridItem>
-
-          <GridItem area={'polls'}>
-            <Box
-              w="100%"
-              borderRadius="2xl"
-              bg="transparent"
-              boxShadow="lg"
-              position="relative"
-              zIndex={2}
-            >
-              <div style={glassLayerStyle} />
-              <VStack pb={1} align="flex-start" position="relative" borderTopRadius="2xl">
-                <div style={glassLayerStyle} />
-                <Text pl={6} fontWeight="bold" fontSize="2xl">
-                  Ongoing Polls
-                </Text>
-              </VStack>
-              <OngoingPolls OngoingPolls={democracyVotingOngoing} />
-            </Box>
-          </GridItem>
-
-          <GridItem area={'leaderboard'}>
-            <Box
-              w="100%"
-              borderRadius="2xl"
-              bg="transparent"
-              boxShadow="lg"
-              position="relative"
-              zIndex={2}
-            >
-              <div style={glassLayerStyle} />
-              <VStack pb={1} align="flex-start" position="relative" borderTopRadius="2xl">
-                <div style={glassLayerStyle} />
-                <Text pl={6} fontWeight="bold" fontSize="2xl">
-                  Leaderboard
-                </Text>
-              </VStack>
-              <Text pl={6} fontSize="lg" mt={2}> Test </Text>
-            </Box>
-          </GridItem>
-
-          <GridItem area={'constitution'}>
-            <Box
-              w="100%"
-              borderRadius="2xl"
-              bg="transparent"
-              boxShadow="lg"
-              position="relative"
-              zIndex={2}
-            >
-              <div style={glassLayerStyle} />
-              <VStack pb={1} align="flex-start" position="relative" borderTopRadius="2xl">
-                <div style={glassLayerStyle} />
-                <Text pl={6} fontWeight="bold" fontSize="2xl">
-                  Constitution
-                </Text>
-              </VStack>
-              <Text pl={6} fontSize="lg" mt={2}> Test </Text>
-            </Box>
-          </GridItem>
-        </Grid>
-      </Box>
-    </>
-  );
+                    </Box>
+                    
+                    <Box mt="2">
+                      <VStack mb="4" mt="2">
+                        <HStack spacing={2} align="center">
+                          <Icon as={FaLink} boxSize={5} />
+                          <Text fontSize="2xl" fontWeight="bold">
+                            About Links
+                          </Text>
+                        </HStack>
+                        {poLinks && poLinks.length > 0 ? (
+                          poLinks.map((link, index) => (
+                            <Text key={index} fontSize="lg">
+                              <Link  fontSize="xl" fontWeight={"bold"} href={link.url} passHref isExternal color="blue.400">
+                                {link.name}
+                              </Link>
+                            </Text>
+                          ))
+                        ) : (
+                          <Text fontSize="lg" mt={2}>No links available</Text>
+                        )}
+                      </VStack>
+                    </Box>
+                    <Box mt="2">
+                      <VStack spacing={2} align="flex-start">
+                        <HStack  spacing={2} align="center">
+                          <Text fontWeight="bold" fontSize="2xl" mt={2}>
+                            Organization Stats:
+                          </Text>
+                        </HStack>
+                        <Text mt="-1" fontSize="lg" ml="8">
+                          Members: 123
+                        </Text>
+                        <Text mt="-1" fontSize="lg" ml="8">
+                          Total Participation Tokens: 45678
+                        </Text>
+                        <Text mt="-1" fontSize="lg" ml="8">
+                          Active Tasks: 7
+                        </Text>
+                        <Text mt="-1" fontSize="lg" ml="8">
+                          Completed Tasks: 12
+                        </Text>
+                        <Text mt="-1" fontSize="lg" ml="8">
+                          Treasury Balance: 12345
+                        </Text>
+                      </VStack>
+                    </Box>
+                  </Grid>
+                </Box>
+              </GridItem>
+      
+              <GridItem area={'tasks'}>
+                <Box
+                  w="100%"
+                  borderRadius="2xl"
+                  bg="transparent"
+                  boxShadow="lg"
+                  position="relative"
+                  zIndex={2}
+                >
+                  <div style={glassLayerStyle} />
+                  <VStack pb={1} align="flex-start" position="relative" borderTopRadius="2xl">
+                    <div style={glassLayerStyle} />
+                    <Text pl={6} fontWeight="bold" fontSize="2xl">
+                      {claimedTasks && claimedTasks.length > 0 ? 'Your Claimed Tasks' : 'Recommended Tasks'}
+                    </Text>
+                  </VStack>
+                  <HStack spacing="3.5%" pb={2} ml={4} mr={4} pt={4}>
+                    {((claimedTasks && claimedTasks.length > 0) ? claimedTasks : reccomendedTasks)?.slice(0, 3).map((task) => (
+                      <Box key={task.id} w="31%" _hover={{ boxShadow: "md", transform: "scale(1.07)"}} p={4} borderRadius="2xl" overflow="hidden" bg="black">
+                        <Link2 href={`/tasks/?task=${task.id}&projectId=${task.projectId}&userDAO=${userDAO}`}>
+                          <VStack textColor="white" align="stretch" spacing={3}>
+                            <Text fontSize="md" lineHeight="99%" fontWeight="extrabold">
+                              {task.id}
+                            </Text>
+                            <HStack justify="space-between">
+                              <Badge colorScheme="yellow">{task.difficulty}</Badge>
+                              <Text fontWeight="bold">Tokens {task.payout}</Text>
+                            </HStack>
+                          </VStack>
+                        </Link2>
+                      </Box>
+                    ))}
+                  </HStack>
+                </Box>
+              </GridItem>
+      
+              <GridItem area={'polls'}>
+                <Box
+                  w="100%"
+                  borderRadius="2xl"
+                  bg="transparent"
+                  boxShadow="lg"
+                  position="relative"
+                  zIndex={2}
+                >
+                  <div style={glassLayerStyle} />
+                  <VStack pb={1} align="flex-start" position="relative" borderTopRadius="2xl">
+                    <div style={glassLayerStyle} />
+                    <Text pl={6} fontWeight="bold" fontSize="2xl">
+                      Ongoing Polls
+                    </Text>
+                  </VStack>
+                  <OngoingPolls OngoingPolls={democracyVotingOngoing} />
+                </Box>
+              </GridItem>
+      
+              <GridItem area={'leaderboard'}>
+                <Box
+                  w="100%"
+                  borderRadius="2xl"
+                  bg="transparent"
+                  boxShadow="lg"
+                  position="relative"
+                  zIndex={2}
+                >
+                  <div style={glassLayerStyle} />
+                  <VStack pb={1} align="flex-start" position="relative" borderTopRadius="2xl">
+                    <div style={glassLayerStyle} />
+                    <Text pl={6} fontWeight="bold" fontSize="2xl">
+                      Leaderboard
+                    </Text>
+                  </VStack>
+                  <Text pl={6} fontSize="lg" mt={2}> Test </Text>
+                </Box>
+              </GridItem>
+      
+              <GridItem area={'constitution'}>
+                <Box
+                  w="100%"
+                  borderRadius="2xl"
+                  bg="transparent"
+                  boxShadow="lg"
+                  position="relative"
+                  zIndex={2}
+                >
+                  <div style={glassLayerStyle} />
+                  <VStack pb={1} align="flex-start" position="relative" borderTopRadius="2xl">
+                    <div style={glassLayerStyle} />
+                    <Text pl={6} fontWeight="bold" fontSize="2xl">
+                      Constitution
+                    </Text>
+                  </VStack>
+                  <Text pl={6} fontSize="lg" mt={2}> Test </Text>
+                </Box>
+              </GridItem>
+            </Grid>
+          </Box>
+        </>
+      );      
 };
 
 export default PerpetualOrgDashboard;
