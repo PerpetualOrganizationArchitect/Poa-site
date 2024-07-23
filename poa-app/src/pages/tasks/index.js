a436afeeb0b4a7abdad15c3ee64193eeb4ea72cc
hudsonhrh <hudsonhrh7@gmail.com>
2024-07-23 16:47:57 -0500

    added loading to tasks page

--- poa-app/src/pages/tasks/index.js
+++ poa-app/src/pages/tasks/index.js
@@ -1,48 +1,37 @@
 import React, { useRef, useEffect } from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Spinner, Center } from '@chakra-ui/react';
 import MainLayout from '../../components/TaskManager/MainLayout';
-//import FloatingBalls from '../components/TaskManager/floatingBalls';
 import { useDataBaseContext } from '@/context/dataBaseContext';
 import { useRouter } from 'next/router';
 import Navbar from "@/templateComponents/studentOrgDAO/NavBar";
-
-
-//<FloatingBalls containerRef={containerRef} />
-
-
-
+import { usePOContext } from '@/context/POContext';
 
 const Tasks = () => {
   const router = useRouter();
   const { userDAO } = router.query;
-
-  const {setTaskLoaded, setSelectedProjectId, projects} = useDataBaseContext();
+  const { setSelectedProjectId, projects } = useDataBaseContext();
+  const { poContextLoading } = usePOContext();
   const containerRef = useRef();
 
-  useEffect(()=>{
-    
-
-
-    if(router.query.projectId!==null){
-      console.log("project id",router.query.projectId)
-      
-      setSelectedProjectId(projects,router.query.projectId)
-
+  useEffect(() => {
+    if (router.query.projectId !== null) {
+      console.log("project id", router.query.projectId);
+      setSelectedProjectId(projects, router.query.projectId);
     }
-    
-
-
-  },[router.query.projectId,projects]);
-
-
+  }, [router.query.projectId, projects]);
 
   return (
     <>
       <Navbar />
-    <Box   minH="90vh" position="relative" bg="blackAlpha.600" ref={containerRef}>
-      
-      <MainLayout />
-    </Box>
+      {poContextLoading ? (
+        <Center height="90vh">
+          <Spinner size="xl" />
+        </Center>
+      ) : (
+        <Box minH="90vh" position="relative" bg="blackAlpha.600" ref={containerRef}>
+          <MainLayout />
+        </Box>
+      )}
     </>
   );
 };
