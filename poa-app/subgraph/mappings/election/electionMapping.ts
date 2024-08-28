1b80aa1d14d2b27511169c5114f1afcf1ab95853
hudsonhrh <hudsonhrh7@gmail.com>
2024-08-28 13:34:57 -0500

    added handler for adding canidate

--- poa-app/subgraph/mappings/election/electionMapping.ts
+++ poa-app/subgraph/mappings/election/electionMapping.ts
@@ -1,8 +1,8 @@
 import { log} from "@graphprotocol/graph-ts"
 import { BigInt } from "@graphprotocol/graph-ts"
-import { ElectionCreated } from "../../generated/templates/ElectionContract/ElectionContract"
+import { ElectionCreated, CandidateAdded } from "../../generated/templates/ElectionContract/ElectionContract"
 
-import { Election, PerpetualOrganization } from "../../generated/schema"
+import { Election, PerpetualOrganization, Candidate } from "../../generated/schema"
 
 
 export function handleElectionCreated(event: ElectionCreated): void {
@@ -14,6 +14,17 @@ export function handleElectionCreated(event: ElectionCreated): void {
     entity.isActive = true;
     entity.electionContract = event.address.toHex();
     
+    entity.save();
+
+}
+
+export function handleCandidateAdded(event: CandidateAdded ): void {
+
+    let entity = new Candidate(event.address.toHex() + "-" + event.params.candidateAddress.toHex());
+    entity.election = event.params.electionId.toHex();
+    entity.candidateAddress = event.params.candidateAddress;
+    entity.candidateName = event.params.candidateName;
+    entity.isWinner = false;
     entity.save();
 
 }
