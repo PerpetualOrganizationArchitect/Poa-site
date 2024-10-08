1fba879d0159154293334643e2b9f6173b401eb6
hudsonhrh <hudsonhrh7@gmail.com>
2024-10-08 16:41:11 -0500

    improved look of message bar

--- poa-app/src/components/Architect/ArchitectInput.jsx
+++ poa-app/src/components/Architect/ArchitectInput.jsx
@@ -1,4 +1,3 @@
-// components/ArchitectInput.jsx
 import React from "react";
 import {
   Input,
@@ -12,29 +11,46 @@ import { ArrowUpIcon } from "@chakra-ui/icons";
 const ArchitectInput = ({ value, onChange, onSubmit, isDisabled }) => {
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
-      onSubmit(); // Execute the onSubmit function if Enter key is pressed
+      onSubmit(); 
     }
   };
+
   return (
-    <Box ml="13%" mr="13%">
-    <InputGroup>
-      <Input
-        value={value}
-        onChange={onChange}
-        placeholder="Type here..."
-        borderColor="blue.900"
-        onKeyDown={handleKeyDown}
-        disabled={isDisabled}
-      />
-      <InputRightElement>
-        <IconButton
-          icon={<ArrowUpIcon color="teal" />}
-          variant="ghost"
-          aria-label="Send"
-          onClick={onSubmit}
+    <Box ml="13%" mr="13%" mt="4" mb="4">
+      <InputGroup>
+        <Input
+          value={value}
+          onChange={onChange}
+          placeholder="Message Poa"
+          borderColor="blue.500"
+          backgroundColor="whiteAlpha.900"
+          borderRadius="full"
+          py={6} 
+          px={4}
+          fontSize="md"
+          transition="border-color 0.2s ease, box-shadow 0.2s ease"  // Smooth transition on focus
+          _placeholder={{ color: "gray.500" }}
+          onKeyDown={handleKeyDown}
+          disabled={isDisabled}
+          boxShadow="md"
+          _focus={{
+            borderColor: "blue.500",
+            boxShadow: "0 0 0 2px rgba(0, 23, 255, 0.3)", // Subtle outer glow on focus
+            outline: "none", // Remove default outline
+          }}
         />
-      </InputRightElement>
-    </InputGroup>
+        <InputRightElement>
+          <IconButton
+            mt="2.5"
+            mr="6"
+            icon={<ArrowUpIcon color="teal.400" />}
+            variant="ghost"
+            aria-label="Send"
+            onClick={onSubmit}
+            _hover={{ background: "teal.50" }}
+          />
+        </InputRightElement>
+      </InputGroup>
     </Box>
   );
 };
